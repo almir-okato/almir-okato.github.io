ac1351482e0f4b6fb5cb57317e93572132460462
Almir Okato <kazunar2@gmail.com>
2020-11-10 23:46:32 -0300

    Adding numvoucher checking

--- live-4stories-pedidos.js
+++ live-4stories-pedidos.js
@@ -11,17 +11,31 @@ const live4storiesPedidos = {
       dataType: 'json'
     });
   },
+  checkvoucher(numvoucher) {
+    return $.ajax({
+      type: 'GET',
+      url: `${apiUrl}/checkvoucher`,
+	  data: {numvoucher: numvoucher},
+      dataType: 'json'
+    });
+  },
   // add a single guestbood entry
-  add(nome, Combo, numvoucher, email, endereco, bairro, telefone, isDelivery, vendedor) {
-    console.log('Sending', nome, Combo, numvoucher, email, endereco, bairro, telefone, isDelivery, vendedor)
+  add(nome, Combo, Refri, numvoucher, email, endereco, bairro, telefone, isDelivery, vendedor) {
+    console.log('Sending', nome, Combo, Refri, numvoucher, email, endereco, bairro, telefone, isDelivery, vendedor)
     return $.ajax({
       type: 'PUT',
       url: `${apiUrl}/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
-        nome, Combo, numvoucher, email, endereco, bairro, telefone, isDelivery, vendedor
+        nome, Combo, Refri, numvoucher, email, endereco, bairro, telefone, isDelivery, vendedor
       }),
       dataType: 'json',
+	  error: function(xhr, status, error){
+		console.log(error);
+        document.body.innerHTML = 'Algo deu errado, por favor tente novamente!<br><a href="javascript:void(0)" onClick="location.reload();">Voltar</a>';
+	    window.scrollTo(0,0);
+	    alert("ERRO! Por favor tente novamente - status " + error + " cod " + xhr.status);
+	  },
     });
   }
 };
@@ -37,23 +51,38 @@ const live4storiesPedidos = {
 		delivery = "Sim"
 	}
 
-    live4storiesPedidos.add(
-      $('#nome').val().trim(),
-      $('#Combo').val().trim(),
-	  $('#numvoucher').val().trim(),
-      $('#email').val().trim(),
-      $('#endereco').val().trim(),
-      $('#bairro').val().trim(),
-      $('#telefone').val().trim(),
-      delivery,
-      $('#vendedor').val().trim()
-    ).done(function(result) {
-	  document.body.innerHTML = 'Pedido feito com sucesso!<br><a href="javascript:void(0)" onClick="location.reload(true);">Voltar</a>';
-	  window.scrollTo(0,0);
-	  alert("Pedido feito com sucesso!");
-    }).error(function(error) {
-      console.log(error);
-    });
+	let count = 0;
+	
+	live4storiesPedidos.checkvoucher(
+		$('#numvoucher').val().trim()
+	).done(function(result) {
+		$.each(result.docs, function(idx, pedido) {
+			count++;
+		});
+		console.log(count);
+		if (count > 0) {
+			alert("Este voucher já está cadastrado! Verifique se o número está correto!");
+			return; //cant add an order
+		}
+
+		live4storiesPedidos.add(
+		  $('#nome').val().trim(),
+		  $('#Combo').val().trim(),
+		  $('#Refri').val().trim(),
+		  $('#numvoucher').val().trim(),
+		  $('#email').val().trim(),
+		  $('#endereco').val().trim(),
+		  $('#bairro').val().trim(),
+		  $('#telefone').val().trim(),
+		  delivery,
+		  $('#vendedor').val().trim()
+		).done(function(result) {
+		  document.body.innerHTML = 'Pedido feito com sucesso!<br><a href="javascript:void(0)" onClick="location.reload(true);">Voltar</a>';
+		  window.scrollTo(0,0);
+		  alert("Pedido feito com sucesso!");
+		});
+	});
+	
   });
   
   // $(document).ready(function() {
